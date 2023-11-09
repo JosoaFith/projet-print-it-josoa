d897030de76c194dc450f39626919111800b81f8
JosoaFith <josoafith@gmail.com>
2023-11-10 00:52:15 +0100

    carousel done
    add dots and fontionalities
    add slides diapo

--- print-it/assets/script.js
+++ print-it/assets/script.js
@@ -19,10 +19,69 @@ const slides = [
 
 const arrowLeft = document.querySelector('#banner .arrow_left'),
 		arrowRight = document.querySelector('#banner .arrow_right');
-arrowLeft.addEventListener('click', (e) => {
-	console.log('left');
-});
-arrowRight.addEventListener('click', (e) => {
-	console.log('right');
+let index = 0;
+let slideTime = null;
+
+
+
+//***************************** Dots *****************************
+for(let slide of slides) {
+	document.querySelector('.dots').insertAdjacentHTML('beforeend', `<div class="dot"></div>`);
+}
+const dots = document.querySelectorAll('.dot');
+dots[index].classList.add('dot_selected');
+const resetDots = () => {
+	for(let dot of dots) {
+		dot.classList.remove('dot_selected');
+	}
+}
+
+// *********************** Dots event click *********************
+dots.forEach((dot, i) => {
+	dots[i].addEventListener('click', (e) => {
+		index = i;
+		slideIndex();
+		slideChange();
+	})
 });
 
+// ********************** Arrows event click ********************
+arrowLeft.addEventListener('click', (e) => {
+	index -= 1;
+	slideIndex();
+	slideChange();
+});
+arrowRight.addEventListener('click', (e) => {
+	index += 1;
+	slideIndex();
+	slideChange();
+});
+
+
+//////////////////////////// FONCTIONS /////////////////////////
+const slideIndex = () => {
+	if(index === slides.length) {
+		index = 0;
+	} else if (index < 0) {
+		index = slides.length - 1;
+	}
+}
+
+const slideChange = () => {
+	document.querySelector('.banner-img').setAttribute('src', `./assets/images/slideshow/${slides[index].image}`);
+	document.querySelector('#banner p').innerHTML = slides[index].tagLine;
+	resetDots();
+	dots[index].classList.add('dot_selected');
+	if(slideTime) {
+		clearTimeout(slideTime);
+	}
+	slideTime = setTimeout(slideTimeOut, 7000);
+}
+
+const slideTimeOut = () => {
+	index += 1;
+	slideIndex();
+	slideChange();
+}
+
+setTimeout(slideTimeOut, 7000);
